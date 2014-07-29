d2c1e54e528c6530fe7a10d8bcdbf927b960c9ed
opsb <oliver@opsb.co.uk>
2014-07-28 23:40:43 -0500

    Initial commit

---
+++ app/router.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: EmberCliIe8DemoENV.locationType
+});
+
+Router.map(function() {
+});
+
+export default Router;
---
+++ tests/helpers/resolver.js
@@ -0,0 +1,9 @@
+import Resolver from 'ember/resolver';
+
+var resolver = Resolver.create();
+
+resolver.namespace = {
+  modulePrefix: 'ember-cli-ie8-demo'
+};
+
+export default resolver;
